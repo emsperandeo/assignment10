c0086ffd99790e9616b6681158143971204a6968
emsperandeo <31972603+emsperandeo@users.noreply.github.com>
2017-11-03 13:57:40 -0400

    Final edit

---
+++ scripts/goats.js
@@ -0,0 +1,11 @@
+function hideit() {
+  this.style.visibility = 'hidden';
+}
+
+document.getElementById("one").onclick = hideit;
+
+document.getElementById("two").onclick = hideit;
+
+document.getElementById("three").onclick = hideit;
+
+document.getElementById("four").onclick = hideit;
